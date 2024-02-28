2cbd9f4dbea387ba2ea085d73af56f515f71c667
gwongibeom <k201gun@kakao.com>
2024-02-28 19:31:02 +0900

    Vue 프로젝트 생성

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,19 @@ export default {
 </script>
 
 <style>
-h2 {
-  color: red;
+@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css');
+:root {
+  font-family: Pretendard Variable;
+  font-weight: 400;
+
+  color-scheme: light dark;
+  color: #191919;
+  background-color: #fff;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
 }
 </style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,3 +1,48 @@
 <template>
-  <header>헤더</header>
+  <header>
+    <img src="@/assets/profile_normal.png" class="profile_pic" />
+    <nav>
+      <a v-for="item of menu" key="item">{{ item }}</a>
+    </nav>
+  </header>
 </template>
+
+<script>
+export default {
+  setup() {
+    return {
+      menu: ['개발', '일상'],
+    }
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+nav {
+  display: flex;
+  gap: 0.3rem;
+  margin-top: 1rem;
+  background-color: #191919;
+  padding: 1rem;
+  border-radius: 9px;
+}
+
+nav a {
+  color: #fff;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+.profile_pic {
+  width: 6rem;
+  min-width: 180px;
+  border-radius: 100vh;
+  border: solid #191919 3px;
+}
+</style>
